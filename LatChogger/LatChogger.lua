d5541aea6a4dcd950a45aa27fe28e9c2562d1b04
KaasToast <raeven05@gmail.com>
2022-02-28 21:43:50 +0100

    LatChogger V1.1

--- LatChogger/LatChogger.lua
+++ LatChogger/LatChogger.lua
@@ -18,74 +18,42 @@ function string:split(inSplitPattern, outResults)
     return outResults
 end
 
-function Islog(line)
-    local pattern = '^%[Info%] '
-    local logtable = line:split(' ')
-    for i, x in pairs(logtable) do
-        if i == 1 then
-            Date1 = x
-        elseif i == 2 then
-            Date = Date1 .. ' ' .. x
-        elseif i == 3 then
-            Rest = x
-        else
-            Rest = Rest .. ' ' .. x
-        end
-    end
-    if string.find(Rest, pattern) ~= nil then
-        local pattern2 = 'host%.$'
-        if string.find(Rest, pattern2) == nil then
-            local pattern3 = 'session$'
-            if string.find(Rest, pattern3) == nil then
-                local pattern4 = 'Info$'
-                if string.find(Rest, pattern4) == nil then
-                    local resttable = Rest:split(' ')
-                    for i, x in pairs(resttable) do
-                        if i == 1 then
-                            Infotag = x
-                        elseif i == 2 then
-                            Message = x
-                        else
-                            Message = Message .. ' ' .. x
-                        end
-                    end
-                    return Message
-                else
-                    return false
-                end
-            else
-                return false
-            end
-        else
-            return false
-        end
-    else
-        return false
-    end
+function IsLog(msg)
+    return string.find(msg, '^%[Info%] ') ~= nil and string.find(msg, 'host%.$') == nil and string.find(msg, 'session$') == nil and string.find(msg, 'Info$') == nil and string.find(msg, ' is spectating ') == nil and string.find(msg, ' is no longer spectating%.$') == nil and string.find(msg, '^Kicking ') == nil and string.find(msg, '^Hooking enabled%.') == nil
 end
 
-function Main()
-    for line in io.lines(LogFile) do
-        for i, x in pairs(Lines) do
-            if x == line then
-                goto continue
+function SendLog(line)
+    Lines[line] = true
+    local msg = nil
+    for i, x in pairs(line:split(' ')) do
+        if i == 3 then
+            msg = x
+        elseif i > 3 then
+            msg = msg .. ' ' .. x
+        end
+    end
+    if IsLog(msg) == true then
+        local message = nil
+        for i, x in pairs(msg:split(' ')) do
+            if i == 2 then
+                message = x
+            elseif i > 2 then
+                message = message .. ' ' .. x
             end
         end
-        if Islog(line) ~= false then
-            table.insert(Lines, line)
-            NETWORK.SEND_CHAT_MESSAGE(Islog(line), false)
-        end
-    ::continue::
+        NETWORK.SEND_CHAT_MESSAGE(message, false)
     end
 end
 
 for line in io.lines(LogFile) do
-    if Islog(line) ~= false then
-        table.insert(Lines, line)
-    end
+    Lines[line] = true
 end
 
 while g_isRunning do
-    Main()
-    g_util.yield()
+    for line in io.lines(LogFile) do
+        if Lines[line] == nil then
+            SendLog(line)
+        end
+    end
+    g_util.yield(100)
 end
